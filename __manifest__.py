105c857b8fbfecfa793951c1773966c30cb52632
gabrieljs26 <gabrieljs26.12@gmail.com>
2026-01-29 13:19:11 +0100

    Fix: Change list to tree view for compatibility

--- __manifest__.py
+++ __manifest__.py
@@ -12,13 +12,11 @@
     'author': "Gabriel",
     'website': "https://www.yourcompany.com",
 
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
+
     'category': 'Human Resources',
     'version': '0.1',
 
-    # any module necessary for this one to work correctly
+  
     'depends': ['base', 'hr', 'calendar'],
 
     # always loaded
@@ -27,9 +25,6 @@
         'views/views.xml',
         'views/templates.xml',
     ],
-    # only loaded in demonstration mode
-    # 'demo': [
-    #     'demo/demo.xml',
-    # ],
+  
 }
 
